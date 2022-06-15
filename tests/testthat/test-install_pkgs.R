d7dbceacb152a5c6c16fbdc96cb0b76e3c56e691
Daniel Sjoberg <danield.sjoberg@gmail.com>
2022-06-14 20:31:24 -0400

    Update test-install_pkgs.R

--- tests/testthat/test-install_pkgs.R
+++ tests/testthat/test-install_pkgs.R
@@ -3,6 +3,7 @@ test_that("install_pkgs() works", {
   expect_error(previous_r_version(), NA)
   expect_error(find_previous_library_loc(), NA)
   expect_error(get_installed_pkgs(), NA)
+  expect_error(walk(letters, print), NA)
 
   expect_true(is_named(list(one = 1, two = 2)))
   expect_false(is_named(list(one = 1, 2)))
